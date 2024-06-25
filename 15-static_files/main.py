ed62cc8c5acaabc8bb4220b93be64630acd1c63a
HTCho1 <sksmsghdxo@naver.com>
2024-06-25 23:36:05 +0900

    Add folders

---
+++ 15-static_files/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+
+# static 폴더를 /static 경로에 마운트
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
